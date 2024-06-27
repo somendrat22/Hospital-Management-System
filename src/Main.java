07c700352df9cb3643ea1ca66a456e19f80f3cf3
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2024-06-27 20:09:03 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Hospital aiims = new Hospital("Aiims New Delhi", 1000, "New Delhi", 123456);
+        //////
 
         aiims.appointDoctor("SOM", "MBBS", 23, "Heart");
         aiims.appointDoctor("TIW", "MBBS", 40, "Brain");
